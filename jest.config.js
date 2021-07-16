95441bff8054b454e6228c678b94183574ef8c6a
papastef <spammenow08@gmail.com>
2021-07-16 09:36:45 -0500

    Rm transformer

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['html'],
 }
